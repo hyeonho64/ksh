f90b035c88c33a60dfa52c57ad86538e04845433
hyeonho <hyeonhokang10@kakao.com>
2023-06-05 22:55:30 +0900

    router 설정.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import './font.css';
+import About from './pages/About';
+import Home from './pages/Home';
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
   );
 }
 
