fa60ed38707c1658f2bf00328fcbff18b177b18a
hyeonho <hyeonhokang10@kakao.com>
2023-06-28 21:58:59 +0900

    about 페이지 개선. 기부 유도 toast 생성.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,67 +1,49 @@
 <template>
   <div class="content">
     <div class="about">
-      <h2>ksh란</h2>
-      <span></span>
+      <h2 style="font-weight: bold; margin-bottom: 1rem;">ksh란</h2>
+      <span><b>K</b>ojan <b>S</b>hooting <b>G</b>ame의 약자로 </span>
     </div>
-    <div class="creator">
-          <div class="card mb-3" style="max-width: 540px;">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img src="" class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">변지우</h5>
-                <p class="card-text">게임 개발. 백엔드 개발.</p>
-                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
+    <div class="developer">
+      <h2 style="font-weight: bold; margin-top: 3rem; margin-bottom: 1rem;">개발진</h2>
+      <div class="card-group">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title" style="font-weight: bold;">강현호</h5>
+            <p class="card-text">프로젝트 총괄. 백앤드 개발.</p>
+            <p class="card-text"><small class="text-body-secondary">링크: <a href="https://github.com/hyeonho64" target="_blank">Github</a>, <a href="https://hyeonho64.vercel.app" target="_blank">Homepage</a></small></p>
           </div>
         </div>
-          <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src="../../public/img/headshot/KakaoTalk_20230621_171747618.jpg" class="img-fluid rounded-start" alt="...">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">강현호</h5>
-                  <p class="card-text">프로젝트 총괄. 프론트앤드 개발. 디자인 개발.</p>
-                  <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src="" class="img-fluid rounded-start" alt="...">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img src="" class="img-fluid rounded-start" alt="...">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                </div>
-              </div>
-            </div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title" style="font-weight: bold;">변지우</h5>
+            <p class="card-text">게임 개발. 동아리 총괄.</p>
+            <p class="card-text"><small class="text-body-secondary">링크: <a href="https://github.com/choungho" target="_blank">Github</a></small></p>
           </div>
         </div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title" style="font-weight: bold;">양건희</h5>
+            <p class="card-text">데이터 관리.</p>
+            <p class="card-text"><small class="text-body-secondary">링크: <a href="https://github.com/keonhee10229" target="_blank">Github</a></small></p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title" style="font-weight: bold;">박준석</h5>
+            <p class="card-text">디자인 개발.</p>
+            <p class="card-text"><small class="text-body-secondary">링크: <a href="https://github.com/choonsik0825" target="_blank">Github</a></small></p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title" style="font-weight: bold;">이도환</h5>
+            <p class="card-text">프론트앤드 개발.</p>
+            <p class="card-text"><small class="text-body-secondary">링크: <a href="https://github.com/365hy" target="_blank">Github</a></small></p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -72,10 +54,6 @@
 }
 
 .about {
-
-}
-
-.creator {
 
 }
 </style>
